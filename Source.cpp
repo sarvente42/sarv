ee9c1566decd123e83662d01f6dc09372de1fb2c
sarvente42 <katianechyporuk07@gmail.com>
2023-03-02 21:06:30 +0200

    done 1 ex

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Your number->>";
+	cin >> x;
+	for (size_t i = 0; i < x; i++)
+	{
+		cout << x << endl; 
+		return 0;
+	}
+}
