40e90f2f39ca8ac8d61b24967fe4db7a0a89bfed
sarvente42 <katianechyporuk07@gmail.com>
2023-03-02 16:57:11 +0200

    done 3 ex

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int length, width, height, sq, volume;
+	cout << "Length(cm)";
+	cin >> length;
+	cout << "Width(cm)";
+	cin >> width;
+	cout << "Height";
+	cin >> height;
+	sq = length * width;
+	volume = sq * height;
+	cout << volume;
+}
